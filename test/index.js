71ad59771b5e52bf45a49ddfeafb3df70a009a71
dschau <dustinschau@gmail.com>
2015-12-29 20:58:03 -0600

    chore: run eslint on test code too

--- test/index.js
+++ test/index.js
@@ -6,6 +6,7 @@ import tasks from './load-tasks';
 describe('load-tasks', () => {
   it('returns object of task/file names', () => {
     const keys = Object.keys(tasks);
+    console.log(keys);
     expect(keys.length).to.be.above(0);
   });
 
--- test/load-tasks.js
+++ test/load-tasks.js
@@ -1,6 +1,10 @@
 import gulpLoadTasks from '../src/';
 
-export default gulpLoadTasks('./test/tasks/*.js', {
+const globs = [
+  './test/tasks/*.js'
+];
+
+export default gulpLoadTasks(globs, {
   src() {},
   pipe() {},
   dest() {}
