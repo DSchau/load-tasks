7a8090aa97653ea79bb58a2301eaa3263997a7f2
dschau <dustinschau@gmail.com>
2015-12-29 18:00:36 -0600

    refactor: switch to for/of loop

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import path from 'path';
 const tasks = {};
 
 export default (pattern, ...args) => {
-  globby.sync(pattern).forEach((file) => {
+  const files = globby.sync(pattern);
+  for ( let file of files ) {
     const name = file.replace(/\.[\w\d]+$/, '').split('/').pop();
     let task = require(path.resolve(file));
     task = task.default ? task.default : task;
     tasks[name] = typeof task === 'function' ? task.apply(this, args) : task;
-  });
+  }
   return tasks;
 };
